583a6c5453fc624d62bda09f5e7817cea5513b13
daidai <dd891688848@163.com>
2025-04-25 12:05:21 +0800

    update

---
+++ dist/assets/Ratio-B4u9w-L7.js
@@ -0,0 +1,2 @@
+import{j as e,T as i}from"./vendor-DXHkIl66.js";import{d as n}from"./index-z4pUiXVL.js";function x({value:t}){const r=n(t).gte(0),o=Number(t).toFixed(2);return e.jsx(i,{fontSize:"12px",fontWeight:"500",color:r?"primary_green":"primary_red",children:r?`+${o}%`:`${o}%`})}export{x as R};
+//# sourceMappingURL=Ratio-B4u9w-L7.js.map
