64cfee6d078951d8bc035b2da1a30343667f8dff
daidai <dd891688848@163.com>
2025-04-25 18:32:50 +0800

    update

---
+++ dist/assets/CheckBox-rv3qIDs9.js
@@ -0,0 +1,2 @@
+import{j as r,F as d}from"./vendor-DXHkIl66.js";import{I as x}from"./index-COrObqhs.js";function p(o){const{checked:e,width:i,height:n,borderRadius:t,onClick:c,cursor:s="pointer"}=o;return r.jsx(d,{align:"center",justifyContent:"center",background:"checked_bg",border:"1px solid",borderColor:"border",cursor:s,width:i||"20px",height:n||"20px",borderRadius:t||"4px",onClick:c,sx:{svg:{fill:"primary !important"}},children:e?r.jsx(x,{xlinkHref:"#icon-icon_check"}):null})}export{p as C};
+//# sourceMappingURL=CheckBox-rv3qIDs9.js.map
