c52ae01bbe072cc5f4e283babcade3d90ddb7213
daidai <dd891688848@163.com>
2025-04-29 14:00:51 +0800

    update

---
+++ dist/assets/IconBg-CCgF-vpH.js
@@ -0,0 +1,2 @@
+import{j as r,F as x}from"./vendor-BulRdKUe.js";import{I as p}from"./index-C9eB4n05.js";function d(o){const{svgHover:e="text_caption",svgFill:t="text_paragraph",xlinkHref:n,onClick:s,variant:i="solid",...a}=o;return r.jsx(x,{bg:"bg_secondary",borderRadius:"12px",w:"40px",h:"40px",alignItems:"center",justifyContent:"center",border:"1px solid",borderColor:"border",cursor:"pointer",...a,onClick:s,children:r.jsx(p,{svgHover:e,svgFill:t,xlinkHref:n,variant:i})})}export{d as I};
+//# sourceMappingURL=IconBg-CCgF-vpH.js.map
