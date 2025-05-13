92a1e8aeae463047f5b0bd00d50e471353ec9893
daidai <dd891688848@163.com>
2025-05-13 16:53:36 +0800

    update

--- dist/assets/swap-D3PlwPKh.js
+++
@@ -1,2 +0,0 @@
-import{Q as n}from"./index-BPmJ2jpN.js";const r=(t,u)=>({byAmountIn:!0,findRouterLoading:!1,fromAmount:"",toAmount:"",fromCoin:void 0,toCoin:void 0,routerData:void 0,setFromCoin:o=>{t(()=>({fromCoin:o}))},setToCoin:o=>{t(()=>({toCoin:o}))},setFromAmount:o=>{t(()=>({fromAmount:o}))},setToAmount:o=>{t(()=>({toAmount:o}))},setRouterData:o=>{t(()=>({routerData:o}))},setFindRouterLoading:o=>{t(()=>({findRouterLoading:o}))},setByAmountIn:o=>{t(()=>({byAmountIn:o}))}}),i=n(r);export{i as u};
-//# sourceMappingURL=swap-D3PlwPKh.js.map
