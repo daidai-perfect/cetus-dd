618a0176653140f5c93e017a1cc36f7fe995b39f
devlb1024 <libin52005@gmail.com>
2025-05-14 11:59:39 +0800

    build

--- dist/assets/CheckBox-GaCpUZnB.js
+++
@@ -1,2 +0,0 @@
-import{j as r,F as d}from"./vendor-BFGDaAdA.js";import{I as x}from"./index-C2McGhUQ.js";function p(o){const{checked:e,width:i,height:n,borderRadius:t,onClick:c,cursor:s="pointer"}=o;return r.jsx(d,{align:"center",justifyContent:"center",background:"checked_bg",border:"1px solid",borderColor:"border",cursor:s,width:i||"20px",height:n||"20px",borderRadius:t||"4px",onClick:c,sx:{svg:{fill:"primary !important"}},children:e?r.jsx(x,{xlinkHref:"#icon-icon_check"}):null})}export{p as C};
-//# sourceMappingURL=CheckBox-GaCpUZnB.js.map
