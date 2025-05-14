435218840785b579637f18ecee3df441482413e4
daidai <dd891688848@163.com>
2025-05-14 16:53:29 +0800

    update

---
+++ dist/assets/index-BmEYB-eP.js
@@ -0,0 +1,2 @@
+import{d as c,p as u,z as i}from"./index-ChSVVHaJ.js";import"./vendor-BFGDaAdA.js";const l=r=>`${c(r).mul(100).toString()}%`,g=(r,s)=>r?r&&!s?!!((r==null?void 0:r.is_verified)===void 0?r!=null&&r.is_trusted:r!=null&&r.is_verified):!!(s!=null&&s.find(d=>{var a,f;return((a=u(d==null?void 0:d.coin_type))==null?void 0:a.full_address)===((f=u((r==null?void 0:r.coin_type)||""))==null?void 0:f.full_address)})):!1,v=(r,s)=>c(r).gt(0)?`${i(r)} ${s}`:"";export{v as f,l as g,g as i};
+//# sourceMappingURL=index-BmEYB-eP.js.map
--- dist/assets/useVaultsHoldings-CCOEFVY-.js
+++
@@ -1,2 +0,0 @@
-import{o as g,s as t,Z as d}from"./index-C2McGhUQ.js";import"./vendor-BFGDaAdA.js";import{i as D}from"./VaultsAprBlock-zPWOXvBS.js";function p(s,e,a,l,u){const{getTokenAmountValueByPyth:i}=D(),{getTokenAmountValue:c}=g(),o=u=="haedal"?i(a,s):c(a,s),n=u=="haedal"?i(l,e):c(l,e),h=t(o,"$"),m=t(n,"$"),r=d(o).plus(n||0).toString(),V=t(r,"$");return{holdCoinAValue:o,holdCoinBValue:n,holdCoinAValueDisplay:h,holdCoinBValueDisplay:m,holdingAmount:r,holdingAmountDisplay:V}}export{p as u};
-//# sourceMappingURL=useVaultsHoldings-CCOEFVY-.js.map
