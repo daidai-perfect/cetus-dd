e10c36b8003f4fc95149e27a8ceed0f1dc6ce0f4
daidai <dd891688848@163.com>
2025-05-21 11:56:03 +0800

    update

---
+++ dist/assets/useDocumentSize-BkQqV9EZ.js
@@ -0,0 +1,2 @@
+import{u as o}from"./index-DoMH3iiU.js";import{a as i}from"./vendor-BowHGuwE.js";function u(){const[t,n]=i.useState({w:document.documentElement.clientWidth,h:document.documentElement.clientHeight}),e=()=>{n({w:document.documentElement.clientWidth,h:document.documentElement.clientHeight})};return o(()=>(window.addEventListener("resize",e),()=>{window.removeEventListener("resize",e)}),[],{wait:500}),{size:t}}export{u};
+//# sourceMappingURL=useDocumentSize-BkQqV9EZ.js.map
