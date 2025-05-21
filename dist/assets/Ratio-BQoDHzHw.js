ec40f3a62906ff8095ea7cb1c1981e2ca5fc80cc
daidai <dd891688848@163.com>
2025-05-21 18:27:32 +0800

    update

--- dist/assets/Ratio-BQoDHzHw.js
+++
@@ -1,2 +0,0 @@
-import{j as e,T as i}from"./vendor-BowHGuwE.js";import{d as n}from"./index-CgoR1qPP.js";function x({value:t}){const r=n(t).gte(0),o=Number(t).toFixed(2);return e.jsx(i,{fontSize:"12px",fontWeight:"500",color:r?"primary_green":"primary_red",children:r?`+${o}%`:`${o}%`})}export{x as R};
-//# sourceMappingURL=Ratio-BQoDHzHw.js.map
