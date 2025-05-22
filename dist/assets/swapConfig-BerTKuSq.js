d326e26ce8b3a5107df3171b1fb5d7646ce392c5
daidai <dd891688848@163.com>
2025-05-22 12:51:41 +0800

    update

--- dist/assets/swapConfig-BerTKuSq.js
+++
@@ -1,2 +0,0 @@
-import{a as o}from"./useSwapHelper-DGtv7AcY.js";import{l as i,aL as p}from"./index-e1XEidi2.js";const S=(r,e)=>({providersSwitchStates:{[o.CETUS]:!0},setProvidersSwitchStates:t=>{const s=e().providersSwitchStates;r(()=>({providersSwitchStates:{...s,...t}}))},supportOrderSplit:!0,setSupportOrderSplit:t=>{r(()=>({supportOrderSplit:t}))}}),d=i(p(S,{name:"useSwapConfigStore"}));export{d as u};
-//# sourceMappingURL=swapConfig-BerTKuSq.js.map
