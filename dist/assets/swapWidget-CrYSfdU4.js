8d947923f16bf4ff6aba6d2336f974748f581310
daidai <dd891688848@163.com>
2025-05-22 18:13:54 +0800

    update

---
+++ dist/assets/swapWidget-CrYSfdU4.js
@@ -0,0 +1,2 @@
+import{l as r,y as n}from"./index-f9FTB66s.js";const m=(t,e)=>({byAmountIn:!0,findRouterLoading:!1,fromAmount:"",toAmount:"",fromCoin:n.clmm_swap.from_coin,toCoin:n.clmm_swap.to_coin,routerData:void 0,setFromCoin:o=>{t(()=>({fromCoin:o}))},setToCoin:o=>{t(()=>({toCoin:o}))},setFromAmount:o=>{t(()=>({fromAmount:o}))},setToAmount:o=>{t(()=>({toAmount:o}))},setRouterData:o=>{t(()=>({routerData:o}))},setFindRouterLoading:o=>{t(()=>({findRouterLoading:o}))},setByAmountIn:o=>{t(()=>({byAmountIn:o}))}}),a=r(m);export{a as u};
+//# sourceMappingURL=swapWidget-CrYSfdU4.js.map
