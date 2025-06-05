32bcf76ecdcdfe7ea38ae4e711230c1a838f4ede
daidai <dd891688848@163.com>
2025-06-05 16:11:41 +0800

    update

--- dist/assets/swapConfig-DmXMmqnq.js
+++
@@ -1,2 +0,0 @@
-import{a as o}from"./useSwapHelper-C9XvcVgO.js";import{l as i,aL as p}from"./index-Bo5tJ0sv.js";const S=(r,e)=>({providersSwitchStates:{[o.CETUS]:!0},setProvidersSwitchStates:t=>{const s=e().providersSwitchStates;r(()=>({providersSwitchStates:{...s,...t}}))},supportOrderSplit:!0,setSupportOrderSplit:t=>{r(()=>({supportOrderSplit:t}))}}),d=i(p(S,{name:"useSwapConfigStore"}));export{d as u};
-//# sourceMappingURL=swapConfig-DmXMmqnq.js.map
---
+++ dist/assets/useDocumentSize-8aI5aQPf.js
@@ -0,0 +1,2 @@
+import{u as o}from"./index-D-f3rLKc.js";import{a as i}from"./vendor-D6laCrlR.js";function u(){const[t,n]=i.useState({w:document.documentElement.clientWidth,h:document.documentElement.clientHeight}),e=()=>{n({w:document.documentElement.clientWidth,h:document.documentElement.clientHeight})};return o(()=>(window.addEventListener("resize",e),()=>{window.removeEventListener("resize",e)}),[],{wait:500}),{size:t}}export{u};
+//# sourceMappingURL=useDocumentSize-8aI5aQPf.js.map
