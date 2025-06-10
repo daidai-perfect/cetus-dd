64f3cfcb1568f80943f38390056abe0f210d518b
daidai <dd891688848@163.com>
2025-06-10 11:30:11 +0800

    update

--- dist/assets/LoadingChart-n3Or6F59.js
+++
@@ -1,2 +0,0 @@
-import{j as r}from"./vendor-B2KdiJr1.js";import"./index-DlTImWSS.js";const m=()=>r.jsx("div",{className:"loader-wrapper",children:r.jsx("div",{className:"loader",children:Array.from({length:9}).map((s,a)=>r.jsx("div",{className:`bar bar-${a+1}`},a))})});export{m as L};
-//# sourceMappingURL=LoadingChart-n3Or6F59.js.map
---
+++ dist/assets/useDocumentSize-DEbMRVlK.js
@@ -0,0 +1,2 @@
+import{u as o}from"./index-BpjJplwy.js";import{a as i}from"./vendor-B2KdiJr1.js";function u(){const[t,n]=i.useState({w:document.documentElement.clientWidth,h:document.documentElement.clientHeight}),e=()=>{n({w:document.documentElement.clientWidth,h:document.documentElement.clientHeight})};return o(()=>(window.addEventListener("resize",e),()=>{window.removeEventListener("resize",e)}),[],{wait:500}),{size:t}}export{u};
+//# sourceMappingURL=useDocumentSize-DEbMRVlK.js.map
