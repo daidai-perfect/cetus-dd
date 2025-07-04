6e37d952045d9151dfc7de452cc85b9378dd56db
daidai <dd891688848@163.com>
2025-07-04 19:35:46 +0800

    update

--- dist/assets/useRpcListener-ZiKvx8ju.js
+++
@@ -1,2 +0,0 @@
-import{ba as r,bk as a}from"./index-B3DWuJbq.js";import{a as n}from"./vendor-B68Ckcdo.js";import{u as o}from"./usePreviousValue-CwObmjBQ.js";function m({onRpcChange:f}){const{isInitLoading:i}=r(),{currentNode:s}=a(),e=o(s),t=n.useRef(!1);n.useEffect(()=>{e&&s&&e.link!==(s==null?void 0:s.link)&&(t.current=!0)},[e==null?void 0:e.link,s==null?void 0:s.link,i]),n.useEffect(()=>{t.current&&!i&&(f(),t.current=!1)},[i,t.current])}export{m as u};
-//# sourceMappingURL=useRpcListener-ZiKvx8ju.js.map
