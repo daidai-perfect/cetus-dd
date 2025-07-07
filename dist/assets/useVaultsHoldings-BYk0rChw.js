db218b06e87ba00a9e08cdf900cb2c3a804a1ef1
daidai <dd891688848@163.com>
2025-07-07 18:03:54 +0800

    update

--- dist/assets/useVaultsHoldings-BYk0rChw.js
+++
@@ -1,2 +0,0 @@
-import{o as g,s as t,a0 as d}from"./index-Cbd3ho5p.js";import{c as D}from"./useGetPythTokenPrice-_4nOg1XH.js";function f(s,e,a,l,u){const{getTokenAmountValueByPyth:c}=D(),{getTokenAmountValue:i}=g(),o=u=="haedal"?c(a,s):i(a,s),n=u=="haedal"?c(l,e):i(l,e),r=t(o,"$"),m=t(n,"$"),h=d(o).plus(n||0).toString(),V=t(h,"$");return{holdCoinAValue:o,holdCoinBValue:n,holdCoinAValueDisplay:r,holdCoinBValueDisplay:m,holdingAmount:h,holdingAmountDisplay:V}}export{f as u};
-//# sourceMappingURL=useVaultsHoldings-BYk0rChw.js.map
