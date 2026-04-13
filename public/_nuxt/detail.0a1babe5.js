077f4e4d6fd59ac2ff0c2bdb37921eb929a2302e
daidai <dd891688848@163.com>
2026-04-13 15:20:55 +0800

    update

---
+++ public/_nuxt/detail.0a1babe5.js
@@ -0,0 +1,1 @@
+import{W as e}from"./entry.5daabe1b.js";const n=e((t,r)=>{const o=document.querySelector(".body-container-bg");o&&o.scrollTo({top:0})});export{n as default};
--- public/_nuxt/detail.ea321a69.js
+++
@@ -1,1 +0,0 @@
-import{W as e}from"./entry.15b54f56.js";const n=e((t,r)=>{const o=document.querySelector(".body-container-bg");o&&o.scrollTo({top:0})});export{n as default};
