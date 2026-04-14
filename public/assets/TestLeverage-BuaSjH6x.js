c31e8f4fa2ebda3da1ed628ebc3242e857c78a9c
daidai <dd891688848@163.com>
2026-04-14 10:22:06 +0800

    update

---
+++ public/assets/TestLeverage-BuaSjH6x.js
@@ -0,0 +1,1 @@
+import{r as t,j as r}from"./vendor-BZDKgC6c.js";import{u as s}from"./useGetSuiLendInfo-D_zTbYBx.js";import"./index--eEZcbiU.js";import"./cross-Bs7tDXXr.js";function u(){const{getLendingMarketData:e}=s();return t.useEffect(()=>{e()},[]),r.jsx("div",{children:"TestLeverage"})}export{u as default};
