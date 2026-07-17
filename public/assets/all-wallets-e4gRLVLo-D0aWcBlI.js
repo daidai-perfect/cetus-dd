8cdaaa7cd4a28000aa5f91580bef6958000d3b58
daidai <dd891688848@163.com>
2026-07-17 20:42:37 +0800

    update

---
+++ public/assets/all-wallets-e4gRLVLo-D0aWcBlI.js
@@ -0,0 +1,6 @@
+import{m as o}from"./solana-D2d9wcV7-YMPGSN7F.js";import"./index-CDkkHeSg.js";import"./Bridge-Dkpi67bB.js";import"./util--bw7gFPs-BWz0kH9e.js";import"./wallets-DeIxpXbE-BD0sp_Fe.js";import"./index.es-DxME80dw-BIKJ_WZ3.js";import"./http-BRrcyI2V-CL-Of5EK.js";const s=o`<svg fill="none" viewBox="0 0 24 24">
+  <path
+    style="fill: var(--wui-color-accent-100);"
+    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
+  />
+</svg>`;export{s as allWalletsSvg};
